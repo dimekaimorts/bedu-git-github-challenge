a1522503e8b35e1d243dd31bae7ca22f71f5f617
dimekaimorts <kaimorts817@gmail.com>
2023-01-13 15:38:27 -0600

    nuevo metodo desde branch QG20624 que pertenece a refactoring

--- test.py
+++ test.py
@@ -4,4 +4,7 @@ class NLBOperation():
     
     def __init__(self):
         print("Hello, world!")
+        
+    def __str__(self):
+        print("Another method added from branch refactoring/QG20624")
     
